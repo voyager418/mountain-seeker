28a30f13ddb1e645e8afff83620148bf83e7cc9d
Voyag3r <george.kg@outlook.com>
2021-02-27 12:55:07 +0100

    Update logging format to put timestamps in the beginning

--- src/logging/log.instance.ts
+++ src/logging/log.instance.ts
@@ -12,20 +12,15 @@ const log = winston.createLogger({
             format: 'DD-MM-YYYY HH:mm:ss'
         }),
         winston.format.colorize(),
-        winston.format.json()
+        winston.format.printf((info: { timestamp: never; level: never; message: never; }) =>
+            `${info.timestamp} ${info.level}: ${JSON.stringify(info.message)}`)
     ),
     transports: [
+        new winston.transports.Console(),
         // Write all logs with level `error` and to `error.log`
         new winston.transports.File({ filename: 'error.log', level: 'error' })
     ],
 });
 
-// If we're not in production then log to the `console` with the format:
-// `${info.level}: ${info.message} JSON.stringify({ ...rest }) `
-if (process.env.NODE_ENV !== 'production') {
-    log.add(new winston.transports.Console({
-        format: winston.format.simple(),
-    }));
-}
 
 export default log;
