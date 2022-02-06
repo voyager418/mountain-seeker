96d4f06ddb6f0111c47380ebee4117f8203808a7
Voyag3r <george.kg@outlook.com>
2022-02-06 21:29:11 +0100

    removed unnecessary step to check status

--- check_deploy.sh
+++ check_deploy.sh
@@ -1,23 +1,7 @@
 #!/bin/bash
 
 aws_host=http://ms-env-2.eba-vmhpnm8k.eu-west-1.elasticbeanstalk.com
-status_endpoint=$aws_host/status
 stop_endpoint=$aws_host/stop
-
-echo "Executing GET request to $status_endpoint"
-status_result=$(curl -s -H "Accept: application/json" -H "Content-Type: application/json" $status_endpoint)
-echo "Response $status_result"
-
-# grep -o -E "\d+" matches all the numbers
-# so if input is {"running":0,"total":5} it will print 2 lines : 0 and 5
-# on Mac grep -Po doesn't work, need to use -o -E
-running=$(echo "$status_result" | grep -Po "\d+" | sed -n '2p') # 2p prints second line
-echo "Running strategies: $running"
-
-if [ "$running" -ne "0" ]; then
-    echo "Exiting as there are $running running strategies"
-    exit 1
-fi
 
 echo "Stopping all strategies..."
 echo "Executing GET request to $stop_endpoint"
@@ -26,6 +10,6 @@ echo "Response $stop_result"
 
 running=$(echo "$stop_result" | grep -Po "\d+" | sed -n '2p')
 if [ "$running" -ne "0" ]; then
-    echo "Exiting as there are $running running strategies (after stopping)"
+    echo "Exiting as $running strategies are still running"
     exit 1
 fi
