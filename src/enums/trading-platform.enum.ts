7810d7208140b5577985efb28196abc0a68b251a
Voyag3r <george.kg@outlook.com>
2022-02-10 19:33:27 +0100

    remove not needed files

--- src/enums/trading-platform.enum.ts
+++
@@ -1,4 +0,0 @@
-export enum TradingPlatform {
-  BINANCE = "binance",
-  COINBASE = "coinbase"
-}
--- src/repository/repository.interface.ts
+++
@@ -1,6 +0,0 @@
-import { Account } from "../models/account";
-
-export interface Repository {
-    /** Creates or updates an account */
-    updateAccount(account: Account): void;
-}
