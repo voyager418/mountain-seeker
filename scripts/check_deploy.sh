de7599bc64de0f815a329bf53946b3c3302e2439
Voyag3r <george.kg@outlook.com>
2022-02-22 20:45:15 +0100

    add a failure if there was an error during HTTP request

--- scripts/check_deploy.sh
+++ scripts/check_deploy.sh
@@ -8,6 +8,11 @@ echo "Executing GET request to $stop_endpoint"
 stop_result=$(curl -s -H "Accept: application/json" -H "Content-Type: application/json" $stop_endpoint)
 echo "Response $stop_result"
 
+if [[ "$stop_result" != *"removed"* ]]; then
+  echo "An exception occurred during HTTP request"
+  exit 1
+fi
+
 # grep -o -E "\d+" matches all the numbers
 # so if input is {"running":0,"total":5} it will print 2 lines : 0 and 5
 # on Mac grep -Po doesn't work, need to use -o -E
