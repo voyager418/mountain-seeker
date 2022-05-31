7a6f5b7e89c63fe05ea198a6b3532833d977ab4c
Voyag3r <george.kg@outlook.com>
2022-05-31 12:32:43 +0200

    decreased min c1variation for strat18-5-5 and strat19-5-10

--- src/strategies/marketselector/msv2/select-by-5min-v4.ts
+++ src/strategies/marketselector/msv2/select-by-5min-v4.ts
@@ -80,7 +80,7 @@ export class SelectBy5minV4 {
             twentyCandlesticksExcept2[twentyCandlesticksExcept2.length - 1][4]));
 
         // if before last candle percent change is below minimal threshold
-        if (c1Variation < 12) {
+        if (c1Variation < 8) {
             return undefined;
         }
 
