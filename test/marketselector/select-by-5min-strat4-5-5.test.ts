fe1e1933d1b3dbf57f91f85db3d736ac783f9dff
Voyag3r <george.kg@outlook.com>
2022-12-26 20:51:49 +0100

    add additional test for strat4-5-5

--- test/marketselector/select-by-5min-strat4-5-5.test.ts
+++ test/marketselector/select-by-5min-strat4-5-5.test.ts
@@ -10,6 +10,11 @@ describe("strat4-5-5 selector", () => {
     const candlesticks = JSON.parse(fs.readFileSync(CURRENT_DIRECTORY_PATH + "/FIDA_5m_candlesticks.json", "utf8")) as Array<TOHLCVF>;
     const candleSticksPercentageVariations = JSON.parse(fs.readFileSync(CURRENT_DIRECTORY_PATH + "/FIDA_5m_candlesticks_percentage_variations.json", "utf8")) as Array<number>;
 
+    let rareCandlesticks = JSON.parse(fs.readFileSync(CURRENT_DIRECTORY_PATH + "/RARE_5m_candlesticks.json", "utf8")) as Array<TOHLCVF>;
+    rareCandlesticks = rareCandlesticks.slice(0, -284);
+    let rareCandleSticksPercentageVariations = JSON.parse(fs.readFileSync(CURRENT_DIRECTORY_PATH + "/RARE_5m_candlesticks_percentage_variations.json", "utf8")) as Array<number>;
+    rareCandleSticksPercentageVariations = rareCandleSticksPercentageVariations.slice(0, -284);
+
     test("Should correctly select", () => {
         const state: MountainSeekerV2State = { accountEmail: "", id: "", marketLastTradeDate: new Map<string, Date>() };
         const market: Market = {
@@ -27,6 +32,22 @@ describe("strat4-5-5 selector", () => {
         expect(res!.edgeVariation).toEqual(1.447);
         expect(res!.maxVariation).toEqual(1.794);
         expect(res!.volumeRatio).toEqual(85.65856980703745);
+    });
+
+    test("Should not select", () => {
+        const state: MountainSeekerV2State = { accountEmail: "", id: "", marketLastTradeDate: new Map<string, Date>() };
+        const market: Market = {
+            candleStickIntervals: [],
+            candleSticks: new Map<CandlestickInterval, Array<TOHLCVF>>(),
+            candleSticksPercentageVariations: new Map<CandlestickInterval, Array<number>>(),
+            originAsset: Currency.BUSD,
+            symbol: "",
+            targetAsset: "",
+            targetAssetPrice: 0,
+            percentChangeLast24h: 3
+        };
+        const res = SelectBy5min.shouldSelectMarket(state, market, "strat4-5-5", false, rareCandlesticks, rareCandleSticksPercentageVariations);
+        expect(res).toBeUndefined(); // because c2 variation is 0.7
     });
 
 });
